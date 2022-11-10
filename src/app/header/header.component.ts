056df8cfc88f819bef6d917e5eb7b5c415daebc7
Bambi <bawe97@gmail.com>
2022-11-10 18:00:28 +0100

    made navbar changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  showNavBg: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.onscroll = () => {
+      this.showNavBackground()
+    };
   }
 
+  showNavBackground() {
+    let prevScrollpos = window.pageYOffset;
+    this.showNavBg = prevScrollpos < 70 ? false : true;
+  }
+  
 }
