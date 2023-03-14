d53a8168ac4ab16f39a3ec36f18c2442df579fc9
Awe_Oluwabambi <oluwabambi.awe@upperlink.ng>
2023-03-14 19:55:38 +0100

    paygate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './paygate/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    window.addEventListener('load', () => {
+      const carousel: any = document.querySelector('.carousel');
+      const images = carousel.querySelectorAll('.carousel-image');
 
+      let counter = 0;
+      const width = images[0].offsetWidth;
+      console.log(width);
+      
+
+      function slide() {
+        const nextIndex = counter % (images.length - 1);
+        carousel.style.transform = `translateX(-${width * nextIndex}px)`;
+      }
+
+      setInterval(() => {
+        counter++;
+        slide();
+      }, 3000);
+    });
+  }
 }
